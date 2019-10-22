362465bed0bfa540e31edb32ef9fcf389479e3dc
Aleksefo <aleksefo@gmail.com>
2019-10-22 13:03:45 +0300

    Redo main screen

--- App.tsx
+++ App.tsx
@@ -8,65 +8,59 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  View,
-  Text,
-  StatusBar, Platform,
-} from 'react-native';
-import RNWhatsAppStickers from "react-native-whatsapp-stickers"
-import { stickerConfig } from "./src/config/stickerConfig"
+import React, { useEffect, useState } from 'react'
+import { View, Text, Platform, TouchableOpacity } from 'react-native'
+import RNWhatsAppStickers from 'react-native-whatsapp-stickers'
+import { stickerConfig } from './src/config/stickerConfig'
 
 const App = () => {
-  const usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
+  const usingHermes =
+    typeof HermesInternal === 'object' && HermesInternal !== null
   const [isWhatsAppAvailable, setIsWhatsAppAvailable] = useState(false)
 
   const { stickers, ...packConfig } = stickerConfig
 
+  useEffect(() => {
+    RNWhatsAppStickers.isWhatsAppAvailable()
+      .then(isWhatsAppAvailable => setIsWhatsAppAvailable(isWhatsAppAvailable))
+      .catch(e => console.log(e))
+  }, [])
 
-    useEffect(() => {
-      RNWhatsAppStickers.isWhatsAppAvailable()
-          .then(isWhatsAppAvailable => {
-            if (isWhatsAppAvailable) {
-              setIsWhatsAppAvailable(true)
-              if (Platform.OS === 'ios') {
-                return RNWhatsAppStickers.createStickerPack(packConfig)
-                    .then(() => {
-                      const promises = stickers.map(item =>
-                          RNWhatsAppStickers.addSticker(item.fileName, item.emojis)
-                      )
-                      Promise.all(promises).then(() => RNWhatsAppStickers.send())
-                    })
-                    .catch(e => console.log(e))
-              }
-
-              return RNWhatsAppStickers.send('myprojectstickers', 'MyProject Stickers')
-            }
-
-            return undefined
-          })
-          .catch(e => console.log(e))
-      }, [])
+  const sendStickers = () => {
+    if (Platform.OS === 'ios') {
+      return RNWhatsAppStickers.createStickerPack(packConfig)
+        .then(() => {
+          const promises = stickers.map(item =>
+            RNWhatsAppStickers.addSticker(item.fileName, item.emojis),
+          )
+          Promise.all(promises).then(() => RNWhatsAppStickers.send())
+        })
+        .catch(e => console.log(e))
+    }
+    return RNWhatsAppStickers.send('myprojectstickers', 'MyProject Stickers')
+  }
 
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          >
-          {!usingHermes ? null : (
-            <View >
-              <Text >Engine: Hermes</Text>
-            </View>
-          )}
-          {isWhatsAppAvailable ? <Text >Available</Text>: <Text >WA Not Available</Text>}
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <View
+      style={{
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'red',
+      }}
+    >
+      <View style={{ flex: 1, borderWidth: 1, borderColor: 'red' }} />
+      <View style={{ alignItems: 'center' }}>
+        <TouchableOpacity
+          onPress={sendStickers}
+          hitSlop={{ top: 12, right: 12, bottom: 12, left: 12 }}
+          disabled={isWhatsAppAvailable}
+          style={{ backgroundColor: isWhatsAppAvailable ? 'purple' : 'gray' }}
+        >
+          <Text>Click Me</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
 
-export default App;
+export default App
