cf556ce56f339616754ef106515b3417f6964cd1
Aleksefo <aleksefo@gmail.com>
2019-10-22 21:00:03 +0300

    Add theme and strings

---
+++ src/values/strings.tsx
@@ -0,0 +1,22 @@
+const str = {
+  // Controls
+  start: 'Start',
+  stop: 'Stop',
+  pause: 'Pause',
+  resume: 'Resume',
+  // Configurator
+  sets: 'SETS',
+  set: 'SET',
+  rounds: 'ROUNDS',
+  timeTotal: 'TOTAL TIME',
+  // Timer
+  round: 'ROUND',
+  timeLeft: 'TIME LEFT',
+  paused: 'PAUSED',
+  // StopAlert
+  stopAlertTitle: 'Are you sure you want to end the session?',
+  stopAlertContinue: 'Continue',
+  stopAlertStop: 'Stop',
+}
+
+export default str
