199561d036c843bafc7d1259060d6a5243cce19a
Aleksefo <aleksefo@gmail.com>
2019-10-22 21:00:36 +0300

    Move Sticker Card to a separate component

--- App.tsx
+++ App.tsx
@@ -1,31 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, { useEffect, useState } from 'react'
-import {
-  View,
-  Text,
-  Platform,
-  TouchableOpacity,
-  Image,
-  FlatList,
-} from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import RNWhatsAppStickers from 'react-native-whatsapp-stickers'
-import { stickerConfig } from './src/config/stickerConfig'
+import colors from './src/values/colors'
+import StickerCard from './src/components/StickerCard'
+import stickerData from './src/values/stickerData'
 
 const App = () => {
-  const usingHermes =
-    typeof HermesInternal === 'object' && HermesInternal !== null
   const [isWhatsAppAvailable, setIsWhatsAppAvailable] = useState(false)
-
-  const { stickers, ...packConfig } = stickerConfig
 
   useEffect(() => {
     RNWhatsAppStickers.isWhatsAppAvailable()
@@ -34,47 +15,8 @@ const App = () => {
   }, [])
 
   const sendStickers = () => {
-    if (Platform.OS === 'ios') {
-      return RNWhatsAppStickers.createStickerPack(packConfig)
-        .then(() => {
-          const promises = stickers.map(item =>
-            RNWhatsAppStickers.addSticker(item.fileName, item.emojis),
-          )
-          Promise.all(promises).then(() => RNWhatsAppStickers.send())
-        })
-        .catch(e => console.log(e))
-    }
-    console.log('App, sendStickers sending')
     return RNWhatsAppStickers.send('pretoria1', 'Elon Musk #1')
   }
-
-  const DATA = [
-    'Pretoria1',
-    'Pretoria2',
-    'Pretoria3',
-    'Pretoria4',
-    'Pretoria5',
-    'Pretoria6',
-    'Pretoria7',
-    'Pretoria8',
-    'Pretoria9',
-    'Pretoria10',
-    'Pretoria11',
-    'Pretoria12',
-    'Pretoria13',
-    'Pretoria14',
-    'Pretoria15',
-  ]
-
-  const Item = ({ picture }) => (
-    <>
-      <Image
-        source={{ uri: `asset:/pretoria1/${picture}.webp` }}
-        style={{ width: 48, height: 48 }}
-      />
-      <Text>{}</Text>
-    </>
-  )
 
   return (
     <View
@@ -82,30 +24,19 @@ const App = () => {
         flex: 1,
         borderWidth: 1,
         borderColor: 'red',
-        backgroundColor: 'green',
+        backgroundColor: colors.background,
       }}
     >
-      <View style={{ flex: 1, borderWidth: 1, borderColor: 'red' }}>
-        <FlatList
-          data={DATA}
-          renderItem={({ item }) => (
-            <Item picture={item} dog={console.log(', item', item)} />
-          )}
-          keyExtractor={(item, index) => index.toString()}
-          style={{
-            borderWidth: 1,
-            borderColor: 'red',
-            backgroundColor: 'pink',
-          }}
-          horizontal
-        />
-      </View>
+      <StickerCard data={stickerData[0]} />
+      <StickerCard data={stickerData[1]} />
       <View style={{ alignItems: 'center' }}>
         <TouchableOpacity
           onPress={sendStickers}
           hitSlop={{ top: 12, right: 12, bottom: 12, left: 12 }}
           disabled={!isWhatsAppAvailable}
-          style={{ backgroundColor: isWhatsAppAvailable ? 'purple' : 'gray' }}
+          style={{
+            backgroundColor: isWhatsAppAvailable ? colors.accent : 'gray',
+          }}
         >
           {isWhatsAppAvailable ? <Text>Click Me</Text> : <Text>Not</Text>}
         </TouchableOpacity>
---
+++ src/components/StickerCard.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from 'react-native'
+
+const StickerCard = ({ data }) => {
+  const Item = ({ picture }) => (
+    <>
+      <Image
+        source={{ uri: `asset:/pretoria1/${picture}.webp` }}
+        style={{ width: 64, height: 64 }}
+      />
+      <Text>{}</Text>
+    </>
+  )
+  return (
+    <View>
+      <View style={{ flexDirection: 'row' }}>
+        <Text>Title</Text>
+        <Text>Author</Text>
+        <Text>Size</Text>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Item picture={item} />}
+          keyExtractor={(item, index) => index.toString()}
+          style={{
+            borderWidth: 1,
+            borderColor: 'red',
+          }}
+          horizontal
+        />
+        <TouchableOpacity
+          onPress={console.log}
+          hitSlop={{ top: 12, right: 12, bottom: 12, left: 12 }}
+          style={{
+            backgroundColor: 'green',
+          }}
+        >
+          <Text>Click Me</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const s = StyleSheet.create({
+  container: {},
+})
+
+export default StickerCard
