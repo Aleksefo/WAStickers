28054700b4682ca261b5a36c5e64eca258f16dae
Aleksefo <aleksefo@gmail.com>
2019-10-22 14:15:50 +0300

    Add basic UI

--- App.tsx
+++ App.tsx
@@ -9,7 +9,14 @@
  */
 
 import React, { useEffect, useState } from 'react'
-import { View, Text, Platform, TouchableOpacity } from 'react-native'
+import {
+  View,
+  Text,
+  Platform,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native'
 import RNWhatsAppStickers from 'react-native-whatsapp-stickers'
 import { stickerConfig } from './src/config/stickerConfig'
 
@@ -37,8 +44,37 @@ const App = () => {
         })
         .catch(e => console.log(e))
     }
-    return RNWhatsAppStickers.send('myprojectstickers', 'MyProject Stickers')
+    console.log('App, sendStickers sending')
+    return RNWhatsAppStickers.send('pretoria1', 'Elon Musk #1')
   }
+
+  const DATA = [
+    'Pretoria1',
+    'Pretoria2',
+    'Pretoria3',
+    'Pretoria4',
+    'Pretoria5',
+    'Pretoria6',
+    'Pretoria7',
+    'Pretoria8',
+    'Pretoria9',
+    'Pretoria10',
+    'Pretoria11',
+    'Pretoria12',
+    'Pretoria13',
+    'Pretoria14',
+    'Pretoria15',
+  ]
+
+  const Item = ({ picture }) => (
+    <>
+      <Image
+        source={{ uri: `asset:/pretoria1/${picture}.webp` }}
+        style={{ width: 48, height: 48 }}
+      />
+      <Text>{}</Text>
+    </>
+  )
 
   return (
     <View
@@ -46,17 +82,32 @@ const App = () => {
         flex: 1,
         borderWidth: 1,
         borderColor: 'red',
+        backgroundColor: 'green',
       }}
     >
-      <View style={{ flex: 1, borderWidth: 1, borderColor: 'red' }} />
+      <View style={{ flex: 1, borderWidth: 1, borderColor: 'red' }}>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => (
+            <Item picture={item} dog={console.log(', item', item)} />
+          )}
+          keyExtractor={(item, index) => index.toString()}
+          style={{
+            borderWidth: 1,
+            borderColor: 'red',
+            backgroundColor: 'pink',
+          }}
+          horizontal
+        />
+      </View>
       <View style={{ alignItems: 'center' }}>
         <TouchableOpacity
           onPress={sendStickers}
           hitSlop={{ top: 12, right: 12, bottom: 12, left: 12 }}
-          disabled={isWhatsAppAvailable}
+          disabled={!isWhatsAppAvailable}
           style={{ backgroundColor: isWhatsAppAvailable ? 'purple' : 'gray' }}
         >
-          <Text>Click Me</Text>
+          {isWhatsAppAvailable ? <Text>Click Me</Text> : <Text>Not</Text>}
         </TouchableOpacity>
       </View>
     </View>
