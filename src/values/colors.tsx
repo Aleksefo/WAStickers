44e1abf2434468cc0ddf0d4f332c45ff04a8f5c8
Aleksefo <aleksefo@gmail.com>
2019-10-22 20:59:39 +0300

    Add colors

---
+++ src/values/colors.tsx
@@ -0,0 +1,9 @@
+export default {
+  primary: '#075E54', // Port Gore
+  accent: '#25D366', // Carrot Orange
+  error: '#D2473C',
+  success: '#34B7F1',
+  text: '#000000',
+  background: '#DCF8C6',
+  white: '#ffffff',
+}
