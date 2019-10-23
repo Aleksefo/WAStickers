8ec72caab061269ed08849d9b9f99eedd7fd86da
Aleksefo <aleksefo@gmail.com>
2019-10-23 14:28:13 +0300

    Add Status and Nav bars

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, StatusBar } from 'react-native'
 import RNWhatsAppStickers from 'react-native-whatsapp-stickers'
 import colors from './src/values/colors'
 import StickerAccordion from './src/components/StickerAccordion'
+import Icon from 'react-native-vector-icons/AntDesign'
+import Theme from './src/values/Theme'
 
 const App = () => {
   const [isWhatsAppAvailable, setIsWhatsAppAvailable] = useState(false)
@@ -15,6 +17,16 @@ const App = () => {
 
   return (
     <View style={st.appContainer}>
+      <StatusBar backgroundColor={colors.primary} barStyle="light-content" />
+      <View style={st.navBar}>
+        <Icon name="hearto" size={40} color={colors.accent} />
+        <Icon
+          name="sharealt"
+          size={40}
+          color={colors.accent}
+          style={{ paddingHorizontal: 30 }}
+        />
+      </View>
       <StickerAccordion isWhatsAppAvailable={isWhatsAppAvailable} />
     </View>
   )
@@ -23,7 +35,21 @@ const st = StyleSheet.create({
   appContainer: {
     flex: 1,
     backgroundColor: colors.background,
-    padding: 10,
+  },
+  navBar: {
+    paddingVertical: 5,
+    width: Theme.w,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    shadowColor: '#000',
+    backgroundColor: colors.primary,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
   },
 })
 
