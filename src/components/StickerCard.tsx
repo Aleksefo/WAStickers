be6f8e66043416e2f0c66bbaec2e0d23a23d3b7f
Aleksefo <aleksefo@gmail.com>
2019-11-16 17:31:03 +0200

    Add RateUs request and related analytics

--- src/components/StickerCard.tsx
+++ src/components/StickerCard.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import { View } from 'react-native'
+import Analytics from 'appcenter-analytics'
+import React, { useState } from 'react'
+import { View, Alert } from 'react-native'
+import Rate from 'react-native-rate'
 import RNWhatsAppStickers from 'react-native-whatsapp-stickers'
 import Sticker from './Sticker'
 import Button from './Button'
 
 const StickerCard = ({ data, title, identifier, isWhatsAppAvailable }) => {
+  const [rated, setRated] = useState(false)
+
   const sendStickers = () => {
-    return RNWhatsAppStickers.send(identifier, title)
+    if (identifier === 'starmanMusk' && !rated) {
+      Alert.alert(
+        'Thank you for using my app',
+        "Unlike similar apps, I'm not going to annoy you with ads, but I'd appreciate if you can rate the app and maybe write a short review :)",
+        [
+          {
+            text: 'No thanks',
+            onPress: () => {
+              Analytics.trackEvent('RateUS request declined')
+              setRated(true)
+              RNWhatsAppStickers.send(identifier, title)
+            },
+            style: 'cancel',
+          },
+          {
+            text: 'Rate and continue',
+            onPress: () => {
+              Analytics.trackEvent('RateUS request accepted')
+              const options = {
+                GooglePackageName: 'com.aleksefo.wastickers',
+              }
+              Rate.rate(options, success => {
+                if (success) {
+                  Analytics.trackEvent('RateUS request succeeded')
+                }
+              })
+              setRated(true)
+            },
+          },
+        ],
+        { cancelable: false },
+      )
+    } else {
+      Analytics.trackEvent('StickerPack activated ' + title)
+      return RNWhatsAppStickers.send(identifier, title)
+    }
   }
 
   return (
